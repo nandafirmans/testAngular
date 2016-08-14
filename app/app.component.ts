203320f7454ca63bbfa3d0d8f3dd2449b1b57182
nandafirmans <nandafirmans@gmail.com>
2016-08-15 02:17:43 +0700

    navbar added

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,21 +1,28 @@
 import {Component, ViewEncapsulation} from "@angular/core";
 import { TimelineComponent } from './tutorial/timeline/timeline.component';
+import { NavbarComponent } from './tutorial/navbar/navbar.component';
 import { Observable } from 'rxjs'; 
 import "rxjs/add/operator/map";
 
 @Component({
   selector : "myApp",
-  styleUrls : ["../assets/css/bootstrap.min.css"],
+  styleUrls : ["../assets/css/bootstrap.min.css", "https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons"],
   template : `
-  <br>
+  <navbar></navbar>
   <div class="container">
-    <div class="row">
-      <br>
+    <div class="row" [ngSwitch]="ViewMode">
+      <template [ngSwitchCase]="'timeline'">
         <timeline></timeline>
+      </template>
+      <template [ngSwitchCase]="'profile'">
+        <h3 class="text-center"> This should be profile page</h3>
+      </template>
     </div>
   </div>
   `,
   encapsulation : ViewEncapsulation.None,
-  directives : [TimelineComponent]
+  directives : [TimelineComponent, NavbarComponent]
 })
-export class AppComponent{ }
+export class AppComponent{
+    ViewMode = "timeline"
+ }
---
+++ app/tutorial/navbar/navbar.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input, Output } from '@angular/core';
+
+@Component({
+    moduleId : module.id,
+    selector : "navbar",
+    templateUrl : "./navbar.html"
+    //template : ``
+})
+export class NavbarComponent{
+    viewMode;
+}
